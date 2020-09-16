f58c7f06689bee3c1bf18c0de48fa6b9f683d3a0
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-16 12:42:00 +0200

    added version 2

--- Blok 1/oefening_12.php
+++ Blok 1/oefening_12.php
@@ -1,5 +1,13 @@
 <?php 
     $names = ['Bram', 'Colin', 'Collin', 'Damian', 'David', 'Dieter', 'Dimitri', 'Frial', 'Jaylani', 'Jesper', 'Jesse', 'Job', 'Jouke', 'Jouri', 'Koen', 'Kyra', 'Manon', 'Mathhew', 'Matthijs', 'Niels', 'Pascal', 'Sander', 'Tim', 'Jessy', 'Jorn', 'Justin'];
+    $name_length = $_GET=['lengte'];
+    $show_names = [];
+    
+    foreach ($names as $value) {
+            if (strlen($value) == $name_length) {
+                array_push($show_names, $value); 
+            }
+    }
 ?>
 
 <!DOCTYPE html>
@@ -7,13 +15,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oefening 2</title>
-</head>
+    <title>Oefening 12</title>
+
 <body>
     <p>
         <?php
-            foreach ($names as $value) {
-                echo $value . '<br />';
+            foreach ($show_names as $value) {
+                $res = $value . '<br />';
+                echo $res;
             };
         ?>
     </p>
