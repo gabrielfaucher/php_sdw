6e1d50ed4d5950b3b11929da007e49343239a8e2
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-16 19:29:12 +0200

    added version 4

--- Blok 1/oefening_12.php
+++ Blok 1/oefening_12.php
@@ -1,12 +1,28 @@
 <?php 
     $names = ['Bram', 'Colin', 'Collin', 'Damian', 'David', 'Dieter', 'Dimitri', 'Frial', 'Jaylani', 'Jesper', 'Jesse', 'Job', 'Jouke', 'Jouri', 'Koen', 'Kyra', 'Manon', 'Mathhew', 'Matthijs', 'Niels', 'Pascal', 'Sander', 'Tim', 'Jessy', 'Jorn', 'Justin'];
-    $name_length = $_GET=['lengte'];
+    $name_length = $_GET['naamlengte'];
     $show_names = [];
-    
-    foreach ($names as $value) {
-            if (strlen($value) == $name_length) {
-                array_push($show_names, $value); 
+
+    if ($name_length == NULL) {
+        $show_names = $names;
+    } else {
+        foreach ($names as $value) {
+            // if (strlen($value) == $name_length) {
+            //     array_push($show_names, $value);
+            //     break;
+            // } 
+            // if (strlen($value) == $name_length) {
+            //     $show_names = ['No names found with that length!'];
+            // }
+            switch (strlen($value) == $name_length) {
+                case 'true':    
+                    array_push($show_names, $value);
+                    break;
+                case 'false':
+                    $show_names = [];
+                    break;
             }
+        }         
     }
 ?>
 
