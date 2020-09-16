fb3546b6ad8bd49451d553d58ede1120150f27ac
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-16 11:06:57 +0200

    added version1

---
+++ Blok 1/oefening_12.php
@@ -0,0 +1,21 @@
+<?php 
+    $names = ['Bram', 'Colin', 'Collin', 'Damian', 'David', 'Dieter', 'Dimitri', 'Frial', 'Jaylani', 'Jesper', 'Jesse', 'Job', 'Jouke', 'Jouri', 'Koen', 'Kyra', 'Manon', 'Mathhew', 'Matthijs', 'Niels', 'Pascal', 'Sander', 'Tim', 'Jessy', 'Jorn', 'Justin'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oefening 2</title>
+</head>
+<body>
+    <p>
+        <?php
+            foreach ($names as $value) {
+                echo $value . '<br />';
+            };
+        ?>
+    </p>
+</body>
+</html>
