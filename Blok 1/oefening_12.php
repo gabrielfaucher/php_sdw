4d55e0d41ac6c958568840b675e5f40d8a98498e
sh0w98 <gpfaucher@st.noorderpoort.nl>
2020-09-17 13:51:56 +0200

    added actual version 4: added sorting

--- Blok 1/oefening_12.php
+++ Blok 1/oefening_12.php
@@ -5,13 +5,14 @@
 
     if ($name_length == NULL) {
         $show_names = $names;
+        sort($show_names);
     } else {
         foreach ($names as $value) {
-            echo strlen($value) == $name_length;
             if (strlen($value) == $name_length) {
                 array_push($show_names, $value);
             }         
         }         
+        sort($show_names);
     }
 ?>
 
@@ -29,7 +30,6 @@
                 echo 'No names found with that length';
             } else {
                 foreach ($show_names as $value) {
-                    var_dump($show_names);
                     $res = $value . '<br />';
                     echo $res;
                 };
