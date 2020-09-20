5a7aa126a5878b1ead153bbcf0ea6c25d97d5f58
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-20 11:50:41 +0200

    completed exercise13 v1

---
+++ Blok 1/oefening_13/version2.php
@@ -0,0 +1,16 @@
+<?php 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Oefening 12</title>
+
+<body>
+    <p>
+        <?php
+        ?>
+    </p>
+</body>
