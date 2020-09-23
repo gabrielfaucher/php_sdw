a7b356910b8299aa7f7724bfa4412324c4796708
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-23 02:43:06 +0200

    add oefening_15 start

--- Blok 1/oefening_14.php
+++ Blok 1/oefening_14.php
@@ -1,63 +1,66 @@
-<?php 
-   // Get user input
-    $min = $_GET['min'];
-    $max = $_GET['max'];
-    $deler = $_GET['deler'];
+<?php
+// Get user input
+$min = $_GET['min'];
+$max = $_GET['max'];
+$deler = $_GET['deler'];
 
-    // Variable declarations
-    $geldig;
-    $getallen = [];
+// Variable declarations
+$geldig;
+$getallen = [];
 
-    // Set $getldig depending on $deler
-    if ($deler == 0) {
-       $geldig = false; 
-    } elseif ($min > $max) {
-        $geldig = false;
-    } else {
-        $geldig = true;
-    }
+// Set $getldig depending on $deler
+if ($deler == 0) {
+   $geldig = false;
+} elseif ($min > $max) {
+   $geldig = false;
+} else {
+   $geldig = true;
+}
 
-    // Add values in range between $min en $max that are divisible by $deler
-    if ($geldig == true) {
-        $range = range($min, $max); 
-        foreach ($range as $value) {
-           if ($value % $deler == 0 ) {
-                array_push($getallen, $value); 
-           } 
-        }
-    }
-   // Count all elements in array
-   $aantal = count($getallen);
+// Add values in range between $min en $max that are divisible by $deler
+if ($geldig == true) {
+   $range = range($min, $max);
+   foreach ($range as $value) {
+      if ($value % $deler == 0) {
+         array_push($getallen, $value);
+      }
+   }
+}
+// Count all elements in array
+$aantal = count($getallen);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Oefening 13</title>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Oefening 13</title>
 </head>
+
 <body>
-    <p>
-       <?php 
-         
-         // Handle exclusions
-         if ($geldig == false) {
-            echo 'Deler mag niet 0 zijn en min mag niet groter zijn dan max! Probeer het opnieuw.';
-         } elseif ($aantal == 0) {
-            echo 'Er zijn geen getallen tussen ' . $min . ' en ' .  $max . ' die deelbaar zijn door ' . $deler . '!';
-         } else {
-            // De volgende ... [aantal] getallen tussen ... [min] en ... [max] zijn deelbaar door ... [deler]: ... [de getallen, gescheiden door komma's]
-            echo 'De volgende ' . $aantal . ' getallen tussen ' . $min . ' en ' . $max . ' zijn deelbaar door ' . $deler . ' :' . '<br />';
-            foreach ($getallen as $key => $value) {
-               if ($value == end($getallen)) {
-                  echo $value;
-               } else {
-                  echo $value . ', ';
-               }
+   <p>
+      <?php
+
+      // Handle exclusions
+      if ($geldig == false) {
+         echo 'Deler mag niet 0 zijn en min mag niet groter zijn dan max! Probeer het opnieuw.';
+      } elseif ($aantal == 0) {
+         echo 'Er zijn geen getallen tussen ' . $min . ' en ' .  $max . ' die deelbaar zijn door ' . $deler . '!';
+      } else {
+         // De volgende ... [aantal] getallen tussen ... [min] en ... [max] zijn deelbaar door ... [deler]: ... [de getallen, gescheiden door komma's]
+         echo 'De volgende ' . $aantal . ' getallen tussen ' . $min . ' en ' . $max . ' zijn deelbaar door ' . $deler . ' :' . '<br />';
+         foreach ($getallen as $key => $value) {
+            if ($value == end($getallen)) {
+               echo $value;
+            } else {
+               echo $value . ', ';
             }
          }
-       ?> 
-    </p>
+      }
+      ?>
+   </p>
 </body>
+
 </html>
---
+++ Blok 1/oefening_15.php
@@ -0,0 +1,43 @@
+<?php
+// Delers
+
+$delers = [2, 3, 5, 7];
+
+// Assosciative array met elke getal en zijn delers
+$delers_per_getal;
+
+// Array met delers waar het getal deelbaar door is
+$delers_getal;
+
+//Je moet gebruik maken van een associatieve array waarmee je het verband legt tussen een getal (de key) en het aantal verschillende delers dat dat getal heeft (de value). Een goede naam voor die array is $delers_per_getal. Je gebruikt de backend om die array te maken. Dat is een kwestie van loopen ($getal = 1 tot en met $getal = 100), en in de loop bepalen hoeveel delers (uit het lijstje 2, 3, 5 en 7) het getal heeft. Aan het eind moet bijvoorbeeld gelden: $delers_per_getal[6] == 2, want 6 is deelbaar door 2 en 3 (2 stuks dus) en niet door 5 en 7.
+
+for ($i = 0; $i <= 100; $i++) {
+   foreach ($delers as $value) {
+      if($i % $value == 0) {
+         $delers_getal = [];
+         array_push($delers_getal, $value);
+      } 
+   }
+   $delers_per_getal = array($i => $delers_getal);
+}
+var_dump($delers_getal);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Oefening 15</title>
+   </head>
+   <body>
+      <p>
+         <?php
+            /* var_dump($delers_per_getal); */
+            /* foreach ($delers_per_getal as $key => $value) { */
+            /*    echo $key; */
+            /* } */
+         ?>
+      </p>
+   </body>
+</html>
