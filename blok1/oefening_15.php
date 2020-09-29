ffe94f5571b57df6753ce79aace15874d0e022a3
sh0w <gpfaucher@st.noorderpoort.nl>
2020-09-29 06:49:14 +0200

    finished backend of ex15

--- blok1/oefening_15.php
+++ blok1/oefening_15.php
@@ -4,23 +4,24 @@
    $delers = [2, 3, 5, 7];
 
    // Assosciative array met elke getal en zijn delers
-   $delers_per_getal;
+   $delers_per_getal = [];
 
    // Array met delers waar het getal deelbaar door is
-   $delers_getal;
+   $delers_getal = [];
 
    //Je moet gebruik maken van een associatieve array waarmee je het verband legt tussen een getal (de key) en het aantal verschillende delers dat dat getal heeft (de value). Een goede naam voor die array is $delers_per_getal. Je gebruikt de backend om die array te maken. Dat is een kwestie van loopen ($getal = 1 tot en met $getal = 100), en in de loop bepalen hoeveel delers (uit het lijstje 2, 3, 5 en 7) het getal heeft. Aan het eind moet bijvoorbeeld gelden: $delers_per_getal[6] == 2, want 6 is deelbaar door 2 en 3 (2 stuks dus) en niet door 5 en 7.
 
-   for ($i = 0; $i <= 100; $i++) {
+   for ($i = 1; $i <= 100; $i++) {
+       $delers_getal = [];
+       $delers_per_getal = [];
        foreach ($delers as $value) {
            if ($i % $value == 0) {
-               $delers_getal = [];
                array_push($delers_getal, $value);
            }
        }
-       $delers_per_getal = array($i => $delers_getal);
+       $delers_per_getal[$i] = count($delers_getal);
    }
-   var_dump($delers_getal);
+   /* var_dump($delers_per_getal); */
 ?>
 
 <!DOCTYPE html>
@@ -33,10 +34,30 @@
    <body>
       <p>
          <?php
-            /* var_dump($delers_per_getal); */
-            /* foreach ($delers_per_getal as $key => $value) { */
-            /*    echo $key; */
-            /* } */
+            echo '<table>';
+
+/* Voor elke tabelrij: (buitenste loop) */
+
+/* { */
+/* echo '<tr>'; */
+
+/* Voor elke tabelkolom: (binnenste loop) */
+
+/* { */
+
+/*    echo '<td>'; */
+
+/* Echo de inhoud van de tabelcel (het getal dat aan de beurt is) */
+
+/*    echo '/<td>'; */
+
+/* } */
+
+/* echo '</tr>'; */
+
+/* } */
+
+/* echo '</table>'; */
          ?>
       </p>
    </body>
