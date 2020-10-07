e29810c544a7d4e61626661211e8a7ae76449d04
sh0w <gpfaucher@st.noorderpoort.nl>
2020-10-07 09:45:51 +0200

    use foreach instead of sort

--- blok1/oefening_11.php
+++ blok1/oefening_11.php
@@ -1,37 +1,52 @@
 <?php
-    $grades = [5, 8, 7, 2, 9, 6, 9];
-    sort($grades);
+// Define $grades & sort
+$grades = [5, 8, 7, 2, 9, 6, 9];
+
+
+$max = 0;
+$min = $grades[0];
+
+// Calculate min & max w/ foreach
+foreach ($grades as $value) {
+    if ($value > $max) {
+        $max = $value;
+    } elseif ($value < $min) {
+        $min = $value;
+    }
+}
+
+// Calculate average
+$average = array_sum($grades) / count($grades);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Oefening 11</title>
-    </head>
-    <body>
-        <p>
-            Cijfers: 
-                <?php
-                    // Output all grades in $grades
-                    foreach ($grades as $value) {
-                        echo $value . ' ';
-                    };
-                ?>
-                <br />
-                <?= 'Aantal cijfers: ' . count($grades) ?>
-                <br /> 
-                <?= 'Hoogste cijfer: ' . end($grades) ?>
-                <br />
-                <?= 'Laagste cijfer: ' . $grades[0] ?>
-                <br />
 
-                <?php
-                    // Calculate & output average grade
-                    $average = array_sum($grades) / count($grades);
-                    echo 'Gemiddelde: ' . $average;
-                ?>
-        </p>
-    </body>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Oefening 11</title>
+</head>
+
+<body>
+  <p>
+    Cijfers:
+    <?php
+    // Output all grades in $grades
+    echo implode(', ', $grades);
+    ?>
+    <br />
+    <?= 'Aantal cijfers: ' . count($grades) ?>
+    <br />
+    <?= 'Hoogste cijfer: ' . $max ?>
+    <br />
+    <?= 'Laagste cijfer: ' . $min ?>
+    <br />
+
+    <?php
+    echo 'Gemiddelde: ' . $average;
+    ?>
+  </p>
+</body>
+
 </html>
