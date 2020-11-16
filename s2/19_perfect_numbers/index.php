d904bd905a2294827a84e6d6fa4c5ae7d1d262e5
Gabriel Faucher <gpfaucher@st.noorderpoort.nl>
2020-11-16 11:06:42 +0100

    fixed ex18 & ex19

--- s2/19_perfect_numbers/index.php
+++ s2/19_perfect_numbers/index.php
@@ -5,33 +5,26 @@ $amount = $_GET['amount'];
 $results = [];
 
 
-// Function to check if a number is perfect 
+// Function to check if a number is a perfect number
 function isPerfectNumber($number)
 {
-    // To store the sum 
     $sum = 0;
-
-    // Traversing through each number 
-    // In the range [1,N) 
     for ($i = 1; $i < $number; $i++) {
         if ($number % $i == 0) {
             $sum = $sum + $i;
         }
     }
 
-    // returns True is sum is equal 
-    // to the original number. 
     return $sum == $number;
 }
 
+// If a number is perfect, add it to $results
 for ($i = 1; $i < $amount; $i++) {
     $res = isPerfectNumber($i);
     if ($res == true) {
         array_push($results, $i);
     }
 }
-
-// Driver's code 
 
 ?>
 
